f4f4a7490f02c814fe02a5da49809a11d239cdd0
vun30 <hoanvu3012@gmail.com>
2025-10-15 19:50:33 +0700

    update analytics for driver

--- src/server.js
+++ src/server.js
@@ -1,14 +1,13 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const http = require("http");
-const connectDB = require('./config/mongodb');
+const connectDB = require("./config/mongodb");
 
-require('dotenv').config();
+require("dotenv").config();
 const errorHandler = require("./middlewares/errorHandler");
 const { setupSocketServer } = require("./sockets");
 
-const payment = require('./controllers/paymentController');
-
+const payment = require("./controllers/paymentController");
 
 const app = express();
 const server = http.createServer(app);
@@ -17,8 +16,9 @@ connectDB();
 // CORS
 app.use(require("./config/cors"));
 // PayOS webhook: PHẢI đặt TRƯỚC express.json()
-app.post('/api/v1/payments/payos/webhook',
-  express.raw({ type: 'application/json' }),
+app.post(
+  "/api/v1/payments/payos/webhook",
+  express.raw({ type: "application/json" }),
   payment.payosWebhook
 );
 
@@ -27,18 +27,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // nếu bạn đôi khi gửi x-www-form-urlencoded
 
 // Routes app
-app.use('/api/v1/auth', require('./routes/v1/authRoutes'));
-app.use('/api/v1/users', require('./routes/v1/userRoutes'));
-app.use('/api/v1/stations', require('./routes/v1/stationsRoutes'));
-app.use('/api/v1/connectors', require('./routes/v1/connectorsRoutes'));
-app.use('/api/v1/wallets', require('./routes/v1/walletRoutes'));
-app.use('/api/v1/tariffs', require('./routes/v1/tariffsRoutes'));
-app.use('/api/v1/payments', require('./routes/v1/paymentRoutes')); // KHÔNG định nghĩa /payos/webhook trong file này
-app.use('/api/v1/bookings', require('./routes/v1/bookingsRoutes'));
-app.use('/api/v1/sessions', require('./routes/v1/sessionsRoutes'));
-app.use('/api/v1/vehicles', require('./routes/v1/vehicleRoutes'));
+app.use("/api/v1/auth", require("./routes/v1/authRoutes"));
+app.use("/api/v1/users", require("./routes/v1/userRoutes"));
+app.use("/api/v1/stations", require("./routes/v1/stationsRoutes"));
+app.use("/api/v1/connectors", require("./routes/v1/connectorsRoutes"));
+app.use("/api/v1/wallets", require("./routes/v1/walletRoutes"));
+app.use("/api/v1/tariffs", require("./routes/v1/tariffsRoutes"));
+app.use("/api/v1/payments", require("./routes/v1/paymentRoutes")); // KHÔNG định nghĩa /payos/webhook trong file này
+app.use("/api/v1/bookings", require("./routes/v1/bookingsRoutes"));
+app.use("/api/v1/sessions", require("./routes/v1/sessionsRoutes"));
+app.use("/api/v1/vehicles", require("./routes/v1/vehicleRoutes"));
 app.use("/api/v1/chargers", require("./routes/v1/chargersRoutes"));
-
+app.use("/api/v1/analytics", require("./routes/v1/analyticsRoutes"));
 
 app.use((req, res) => {
   res.status(404).json({
@@ -50,7 +50,6 @@ app.use((req, res) => {
 
 // Error handler (phải đặt cuối cùng)
 app.use(errorHandler);
-
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
