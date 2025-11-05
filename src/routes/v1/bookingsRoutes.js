aca96c696e6a5b772a09ceec220a318dce2bf312
vun30 <hoanvu3012@gmail.com>
2025-11-05 17:20:01 +0700

    update

--- src/routes/v1/bookingsRoutes.js
+++ src/routes/v1/bookingsRoutes.js
@@ -6,10 +6,15 @@ const { ROLES } = require('../../constants/enums');
 const mod = require('../../controllers/bookingsController'); // file bạn gửi
 const controller = mod.default || mod;
 
-router.use(protect([ROLES.ADMIN]));
+router.use(protect([ROLES.DRIVER, ROLES.ADMIN, ROLES.STAFF]));
 
 router.get('/', controller.listBookings);              // GET /api/v1/admin/bookings
 router.get('/:id', controller.getBooking);             // GET /api/v1/admin/bookings/:id
 router.patch('/:id/status', controller.updateBookingStatus); // PATCH /api/v1/admin/bookings/:id/status
+
+router.post("/", bookingsController.createBooking);
+router.get("/me", bookingsController.getMyBookings);
+router.get("/available-slots", bookingsController.getAvailableSlots);
+router.delete("/:id", bookingsController.cancelBooking);
 
 module.exports = router;
